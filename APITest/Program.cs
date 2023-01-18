cc087b3aa3770f4a813c882ae5f810e991374e7f
rahimvaliyev <rahimvaliyev99@gmail.com>
2023-01-18 13:54:14 +0400

    Add project files.

---
+++ APITest/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace APITest
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string ApiUrl = "https://www.boredapi.com/api/activity";
+            HttpClient client = new HttpClient();
+
+           
+            bool isContionue;
+            do
+            {
+                var result = await client.GetStringAsync(ApiUrl);
+               var BoredActivity =JsonConvert.DeserializeObject<Bored>(result);
+
+                Console.WriteLine(BoredActivity.activity);
+
+                Console.WriteLine("Davaam etmek isdiyirsize : true/false yazin");
+                isContionue = Convert.ToBoolean(Console.ReadLine());
+            } while (true);
+
+        }
+        class Bored
+        {
+            public string activity { get; set; }  
+        }
+    }
+}
